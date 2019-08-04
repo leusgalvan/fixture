c9d8d6381911e71d9455cc8439d91ad4db03266e
Leandro Galvan <leusgalvan@gmail.com>
2019-08-04 19:48:50 -0300

    Wait for firebase to initialize

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,7 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import SignIn from '../SignIn';
 import { withStyles } from '@material-ui/core/styles';
-import { Typography } from '@material-ui/core';
+import { Typography, LinearProgress } from '@material-ui/core';
 import { FirebaseContext } from '../Firebase';
 import MainMenu from '../MainMenu';
 
@@ -9,15 +9,24 @@ const styles = theme => ({
     app: {
         margin: theme.spacing(3),
     },
+
+    loader: {
+        margin: "auto",
+    }
 });
 
 const App = ({ classes }) => {
     const firebase = useContext(FirebaseContext);
+    const [initialized, setInitialized] = useState(false);
+    useEffect(() => {
+        firebase.auth.onAuthStateChanged(setInitialized);
+    }, []);
     const [user, setUser] = useState(firebase.auth.currentUser);
     const [error, setError] = useState('');
     return (
         <div className={classes.app}>
-            {!user && !error && <SignIn onLoginError={setError} onLoginSuccess={setUser} />}
+            {!initialized && <LinearProgress />}
+            {initialized && !user && !error && <SignIn onLoginError={setError} onLoginSuccess={setUser} />}
             {error && (<Typography variant="body1" color="textPrimary">{error}</Typography>)}
             {user && (<MainMenu />)}
         </div>
