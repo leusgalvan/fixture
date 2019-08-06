a56ee16a9683cb9ef62cd7de7bca23a44ff49c80
Gino Emiliozzi <gino.emiliozzi@jemstep.com>
2019-08-06 17:00:27 -0300

    Now can generate tournaments with odd number of teams

--- src/components/Tournament/createTournament.js
+++ src/components/Tournament/createTournament.js
@@ -1,8 +1,15 @@
 import _ from 'lodash'
 
 const createTournament = selectedTeams => {
-  if (selectedTeams.length === 0) return []
-  const shuffledTeams = _.shuffle(selectedTeams)
+  if (selectedTeams.length < 2) return []
+  var shuffledTeams
+  if (selectedTeams.length % 2 != 0) {
+    shuffledTeams = _.shuffle(
+      selectedTeams.concat({ label: 'LIBRE', value: 'LIBRE' })
+    )
+  } else {
+    shuffledTeams = _.shuffle(selectedTeams)
+  }
   const nrDates = shuffledTeams.length - 1
 
   const upper = shuffledTeams.slice(0, shuffledTeams.length / 2)
