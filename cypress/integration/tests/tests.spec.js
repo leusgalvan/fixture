31534792ae6fc54498faf5378b99984f8463a904
juan9728 <juanpromanzio@gmail.com>
2019-08-10 21:07:53 -0300

    disabled videos and fixtures

--- cypress/integration/tests/tests.spec.js
+++ cypress/integration/tests/tests.spec.js
@@ -1,6 +1,16 @@
+const componentsLoginPage = {
+    logInButton : '//*[@id="root"]/div/div/button/span[1]',
+    title: '//*[@id="root"]/div/div/h5'
+};
+
 describe('Login Tests', () => {
+
     it('Check title', () => {
         cy.visit('http://localhost:3000/');
-        cy.xpath('//*[@id="root"]/div/div/h5').should('contain', 'Welcome');
-    })
+        cy.xpath(componentsLoginPage.title).should('contain', 'Welcome');
+    });
+
+    it('Check button classs', () => {
+        cy.xpath(componentsLoginPage.logInButton).should('have.class', 'MuiButton-label');
+    });
 });
