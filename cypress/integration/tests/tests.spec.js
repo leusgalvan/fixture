9f6d19ef92305db04e27dc68e37c9d2d15c68237
Leandro Galvan <leusgalvan@gmail.com>
2019-08-15 00:54:33 -0300

    Configure Cypress to work on port 5000

--- cypress/integration/tests/tests.spec.js
+++ cypress/integration/tests/tests.spec.js
@@ -1,16 +1,21 @@
 const componentsLoginPage = {
-    logInButton : '//*[@id="root"]/div/div/button/span[1]',
-    title: '//*[@id="root"]/div/div/h5'
+  logInButton: '//*[@id="root"]/div/div/button/span[1]',
+  title: '//*[@id="root"]/div/div/h5'
 };
 
-describe('Login Tests', () => {
+describe("Login Tests", () => {
+  const port = process.env.PORT || "5000";
+  const url = `http://localhost:${port}/`;
 
-    it('Check title', () => {
-        cy.visit('http://localhost:3000/');
-        cy.xpath(componentsLoginPage.title).should('contain', 'Welcome');
-    });
+  it("Check title", () => {
+    cy.visit(url);
+    cy.xpath(componentsLoginPage.title).should("contain", "Welcome");
+  });
 
-    it('Check button classs', () => {
-        cy.xpath(componentsLoginPage.logInButton).should('have.class', 'MuiButton-label');
-    });
+  it("Check button classs", () => {
+    cy.xpath(componentsLoginPage.logInButton).should(
+      "have.class",
+      "MuiButton-label"
+    );
+  });
 });
