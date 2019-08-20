13323c0c707db1363cc12a132ea918db6d4397cc
Gino Emiliozzi <ginoemiliozzi11@gmail.com>
2019-08-20 16:31:57 -0300

    Filter standing when the team has no members

--- src/components/Standings/mapTournamentToStandings.ts
+++ src/components/Standings/mapTournamentToStandings.ts
@@ -14,38 +14,40 @@ export default function mapTournamentToStandings(
     return matchesSoFar.concat(matchDay.matches);
   }, []);
 
-  const standings = matches.reduce<Standing[]>((standingsSoFar, match) => {
-    const standingsWithNoGames: Standing[] = match.teams
-      .filter(
-        team => !standingsSoFar.some(standing => standing.teamId === team.id)
-      )
-      .map(team => ({
-        teamId: team.id,
-        teamName: team.name,
-        score: 0,
-        gamesPlayed: 0,
-      }));
+  const standings = matches
+    .filter(m => m.teams.every(t => t.members.length > 0))
+    .reduce<Standing[]>((standingsSoFar, match) => {
+      const standingsWithNoGames: Standing[] = match.teams
+        .filter(
+          team => !standingsSoFar.some(standing => standing.teamId === team.id)
+        )
+        .map(team => ({
+          teamId: team.id,
+          teamName: team.name,
+          score: 0,
+          gamesPlayed: 0,
+        }));
 
-    const newStandingsSoFar: Standing[] = [
-      ...standingsSoFar,
-      ...standingsWithNoGames,
-    ];
+      const newStandingsSoFar: Standing[] = [
+        ...standingsSoFar,
+        ...standingsWithNoGames,
+      ];
 
-    const maybeWinnerTeamId = match.result;
-    return newStandingsSoFar.map(standing => {
-      const deltaGamesPlayed =
-        match.teams.map(team => team.id).includes(standing.teamId) &&
-        maybeWinnerTeamId !== "not played"
-          ? 1
-          : 0;
-      const deltaScore = standing.teamId === maybeWinnerTeamId ? 1 : 0;
-      return {
-        ...standing,
-        score: standing.score + deltaScore,
-        gamesPlayed: standing.gamesPlayed + deltaGamesPlayed,
-      };
-    });
-  }, []);
+      const maybeWinnerTeamId = match.result;
+      return newStandingsSoFar.map(standing => {
+        const deltaGamesPlayed =
+          match.teams.map(team => team.id).includes(standing.teamId) &&
+          maybeWinnerTeamId !== "not played"
+            ? 1
+            : 0;
+        const deltaScore = standing.teamId === maybeWinnerTeamId ? 1 : 0;
+        return {
+          ...standing,
+          score: standing.score + deltaScore,
+          gamesPlayed: standing.gamesPlayed + deltaGamesPlayed,
+        };
+      });
+    }, []);
 
   const sortedStandings = standings.sort((a, b) => b.score - a.score);
 
