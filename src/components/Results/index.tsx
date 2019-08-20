d2c2db8d69a323ab06686b6c5ee8edefaad2c9ca
Leandro Galvan <leusgalvan@gmail.com>
2019-08-20 16:54:51 -0300

    Improve add button

--- src/components/Results/index.tsx
+++ src/components/Results/index.tsx
@@ -5,7 +5,7 @@ import { Typography, Grid, Button } from "@material-ui/core";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import { makeStyles } from "@material-ui/core/styles";
 import { RouteComponentProps } from "react-router";
-import { Tournament, MatchDay } from "../../types";
+import { Tournament } from "../../types";
 
 const useStyles = makeStyles({
   root: {
--- src/components/SignIn/index.tsx
+++ src/components/SignIn/index.tsx
@@ -1,6 +1,5 @@
 import React, { useContext, useState } from "react";
 import Button from "@material-ui/core/Button";
-import Paper from "@material-ui/core/Paper";
 import { makeStyles } from "@material-ui/core/styles";
 import { Typography, CircularProgress, Card } from "@material-ui/core";
 import { FirebaseContext } from "../Firebase";
