7b536cf3b9ce68e31124c8d36630595be086a4cd
Gino Emiliozzi <ginoemiliozzi11@gmail.com>
2019-08-26 14:36:41 -0300

    Add tooltip for members in tournament view

--- src/components/Tournament/TeamView.tsx
+++ src/components/Tournament/TeamView.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Paper, Typography, ButtonBase } from "@material-ui/core";
+import { Paper, Typography, ButtonBase, Tooltip } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 import { Theme } from "../../theme";
+import { Team } from "../../types";
 
 const useStyles = makeStyles<Theme, Pick<TeamViewProps, "winner">>(theme => {
   return {
@@ -16,18 +17,25 @@ const useStyles = makeStyles<Theme, Pick<TeamViewProps, "winner">>(theme => {
 });
 
 interface TeamViewProps {
-  teamName: string;
+  team: Team;
   winner: boolean;
   onTeamSelected: () => void;
 }
 
-const TeamView = ({ teamName, winner, onTeamSelected }: TeamViewProps) => {
+const TeamView = ({ team, winner, onTeamSelected }: TeamViewProps) => {
   const classes = useStyles({ winner });
 
+  const membersName = team.members
+    .map(m => m.displayName)
+    .reduce((prev, curr) => {
+      return `${prev}  ${curr}`;
+    }, "Members: ");
   return (
     <Paper className={classes.teams}>
       <ButtonBase onClick={onTeamSelected}>
-        <Typography variant="h5">{teamName}</Typography>
+        <Tooltip title={membersName}>
+          <Typography variant="h5">{team.name}</Typography>
+        </Tooltip>
       </ButtonBase>
       {winner && <Typography variant="h6">Winner</Typography>}
     </Paper>
